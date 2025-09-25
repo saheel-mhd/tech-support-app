e759b98f97bf2923299770794180581c397a59c4
muhammed saheel <137157119+saheel-mhd@users.noreply.github.com>
2025-09-25 16:44:51 +0400

    .env change

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ import ticketRoutes from "./routes/ticketRoutes.js";
 import chatRoutes from "./routes/chatRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 
-
 dotenv.config();
 connectDB();
+const API_URL = process.env.VITE_APP_BASE_URL;
 
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", // Vite frontend URL
+  origin: API_URL || "http://localhost:5173", // Vite frontend URL
   credentials: true,
 }));
 
@@ -27,7 +27,7 @@ app.use("/api/tickets", ticketRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/users", userRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
